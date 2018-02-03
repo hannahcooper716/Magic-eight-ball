feab73138938445f1bc50e7a68243ef8391dd59d
Hannah Cooper <hannahcooper716@MacBook.localdomain>
2018-02-02 20:12:11 -0500

    new

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,7 @@
-def function():
-    question = input('What is your question?: ')
+user = input('What is your question?: ')
+while user != 'quit':
+    def function():
+        question = input('What is your question?: ')
+    def check_question():
+        if user[-1] =! '?':
+            print('I’m sorry, I can only answer questions')
